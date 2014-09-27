24a69ae76b0dde10ead463e2b6585b90917fc5ee
a3875180 <a3875180@trbvm.com>
2014-09-27 17:49:25 +0900

    新規クラス追加

---
+++ src/test/java/GitHelloWorld2/GitHelloWorld2/MainTest.java
@@ -0,0 +1,14 @@
+package GitHelloWorld2.GitHelloWorld2;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class MainTest extends TestCase {
+	@Test
+	public void test(){
+		assertThat(new Main().test(), is("test"));
+	}
+}
